9b7d445f6f5fce1f917ce13f49ab072807bc7f4d
Matheus Xavier Silva <matheus.x@mobilitee.com.br>
2019-10-29 16:19:15 -0300

    Add some documentation and change from linq queries to extended logic

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using static ImmutableSharp.ImmutableSharp;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tests
@@ -9,12 +9,18 @@ namespace Tests
     {
         static void Main(string[] args)
         {
+            // These examples show function chaining and immutability
             var a = new int[] {0, 1, 2, 3, 4, 5, 6, 7};
             var b = a.Map((x => x + 5));
             var c = a.Map((x => x));
+            var d = a.Map((x => x + 5)).Filter((x => x > 10));
+            var e = new List<int> (a);
+            var f = e.Map((x => x * 100)).Filter((x => x > 200)).MapCast<int, string>((x => x.ToString()));
             Console.WriteLine("a [{0}] typeof a is {1}", string.Join(", ", a), a.GetType());
             Console.WriteLine("b [{0}] typeof b is {1}", string.Join(", ", b), b.GetType());
             Console.WriteLine("c [{0}] typeof c is {1}", string.Join(", ", c), c.GetType());
+            Console.WriteLine("d [{0}] typeof d is {1}", string.Join(", ", d), d.GetType());
+            Console.WriteLine("f [{0}] typeof f is {1}", string.Join(", ", f), f.GetType());
             Console.WriteLine("a' [{0}]", string.Join(", ", a));
             Console.WriteLine("a == c is {0}", arg0: (a.SequenceEqual(c)));
         }
--- immutableSharp/ImmutableSharp.cs
+++ immutableSharp/ImmutableSharp.cs
@@ -1,18 +1,70 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace ImmutableSharp
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class ImmutableSharp
     {
         /// <summary>
         /// Iterates over an IEnumerable and apply a function to a copy of it
         /// </summary>
-        /// <param name="functor">The IEnumerable to iterate</param>
+        /// <param name="functor">The IEnumerable to iterate over`</param>
         /// <param name="function">The fuction to apply to each item</param>
         /// <typeparam name="T">Must be IEnumerable</typeparam>
-        /// <returns>Returns a modified copy of the original functor</returns>
-        public static T[] Map<T>(this T[] functor, Func<T, T> function) => functor.Select(function).ToArray();
-        public static T[] Filter<T>(this T[] functor, Func<T, bool> function) => functor.Where(function).ToArray();
+        /// <returns>Returns a modified by the function copy of the original functor</returns>
+        public static T[] Map<T>(this IEnumerable<T> functor, Func<T, T> function) 
+        {
+            // Create list to copy the mapped enumerable to
+            var functorCopy = new List<T> ();
+
+            // Apply the function to each item in enumerable
+            foreach(var item in functor) 
+                functorCopy.Add(function(item));
+            
+            // Return the copy as an array
+            return functorCopy.ToArray();
+        }
+
+        /// <summary>
+        /// Mapper that can return a different type than the functor's type
+        /// </summary>
+        /// <param name="functor"></param>
+        /// <param name="function"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="I"></typeparam>
+        /// <returns></returns>
+        public static I[] MapCast<T, I>(this IEnumerable<T> functor, Func<T, I> function) 
+        {
+            // Create list to copy the mapped enumerable to
+            var functorCopy = new List<I> ();
+
+            // Apply the function to each item in enumerable
+            foreach(var item in functor) 
+                functorCopy.Add(function(item));
+            
+            // Return the copy as an array
+            return functorCopy.ToArray();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="functor"></param>
+        /// <param name="function"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T[] Filter<T>(this T[] functor, Func<T, bool> function)
+        {
+            var functorCopy = new List<T> ();
+
+            foreach(var item in functor)
+                if (function(item))
+                    functorCopy.Add(item);
+
+            return functorCopy.ToArray();
+        }
     }
 }
